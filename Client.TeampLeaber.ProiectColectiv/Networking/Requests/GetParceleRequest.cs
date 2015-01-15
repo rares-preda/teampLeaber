b7e1145758861d10bb03b4d9688fa8d25cee7ccb
Rares Mihai Preda <rares.preda@rodeapps.com>
2015-01-15 03:44:31 +0200

    morminte, cimitire, parcele.

---
+++ Client.TeampLeaber.ProiectColectiv/Networking/Requests/GetParceleRequest.cs
@@ -0,0 +1,47 @@
+﻿using Client.TeampLeaber.ProiectColectiv.Models;
+using Client.TeampLeaber.ProiectColectiv.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.TeampLeaber.ProiectColectiv.Networking.Requests
+{
+    public class GetParceleRequest : BaseRequest
+    {
+        private int _idCimitir;
+
+        public GetParceleRequest(int idCimitir)
+        {
+            _idCimitir = idCimitir;
+        }
+
+        public async Task<List<ParcelaModel>> Run()
+        {
+            try
+            {
+                response = await this.GetAsync(Constants.ParcelePath + "/GetByCimitir?cimitirId=" + this._idCimitir);
+
+                if (response.StatusCode == HttpStatusCode.OK || response.StatusCode == HttpStatusCode.Accepted) // 200
+                {
+                    List<ParcelaModel> parcele = await response.Content.ReadAsAsync<List<ParcelaModel>>();
+                    return parcele;
+                }
+                else
+                {
+                    ErrorModel error = await response.Content.ReadAsAsync<ErrorModel>();
+                    ErrorHandling.ErrorHandling.Instance.HandleErrors(error.errors);
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                ErrorHandling.ErrorHandling.Instance.HandleError(Constants.ErrorMessages.Unknown_error);
+                return null;
+            }
+        }
+    }
+}
--- Client.TeampLeaber.ProiectColectiv/Networking/Requests/StergeCimitireRequest.cs
+++ Client.TeampLeaber.ProiectColectiv/Networking/Requests/StergeCimitireRequest.cs
@@ -1,39 +1,45 @@
-﻿using Client.TeampLeaber.ProiectColectiv.Models;
-using Client.TeampLeaber.ProiectColectiv.Utils;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+using Client.TeampLeaber.ProiectColectiv.Models;
+using Client.TeampLeaber.ProiectColectiv.Utils;
 
 namespace Client.TeampLeaber.ProiectColectiv.Networking.Requests
 {
-    public class CimitireRequest : BaseRequest
+    public class StergeCimitireRequest : BaseRequest
     {
-        public async Task<List<CimitirModel>> Run()
+        private int id;
+        public StergeCimitireRequest(int id)
+        {
+            this.id = id;
+        }
+
+        internal async Task<bool> Run()
         {
             try
             {
-                response = await this.GetAsync(Constants.CimitirePath);
+                response = await this.DeleteAsync(Constants.CimitirePath + "/" + id);
 
-                if (response.StatusCode == HttpStatusCode.OK || response.StatusCode == HttpStatusCode.Accepted) // 200
+                if (response.StatusCode == HttpStatusCode.OK) // 200
                 {
-                    List<CimitirModel> cimitire = await response.Content.ReadAsAsync<List<CimitirModel>>();
-                    return cimitire;
+                    return true;
                 }
                 else
                 {
                     ErrorModel error = await response.Content.ReadAsAsync<ErrorModel>();
                     ErrorHandling.ErrorHandling.Instance.HandleErrors(error.errors);
-                    return null;
+                    return false;
                 }
             }
             catch (Exception)
             {
                 ErrorHandling.ErrorHandling.Instance.HandleError(Constants.ErrorMessages.Unknown_error);
-                return null;
+                return false;
             }
         }
     }
